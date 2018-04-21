c251d4157ea3f54c79377e326bc70e69bcc4dbf3
Lyubomir <lubeto3099@gmail.com>
2018-04-22 00:53:37 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from distutils.core import setup
 import sys
+import os
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
 
 setup(
     name='pyinputedittext',
@@ -8,15 +14,17 @@ setup(
     install_requires=["pywin32==223"] if sys.platform.startswith("win") else [],
     version='1.0',
     description='Cross-platform package that allows editing text in console',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     author='Lyubomir Rumenov',
     author_email='lubeto3099@gmail.com',
     url='https://github.com/Def4l71diot/pyinputedittext',
     download_url='https://github.com/Def4l71diot/pyinputedittext/archive/1.0.tar.gz',
     keywords=['linux', 'windows', 'macos', 'unix', 'console', 'input', 'development'],
     classifiers=[
-        'Development Status :: Production/Stable',
+        'Development Status :: Stable',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: CLI',
+        'Topic :: Software Development',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
     ]
